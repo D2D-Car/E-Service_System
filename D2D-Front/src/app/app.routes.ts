f46b46972afe43ec30979eef26e9eea350dc8b38
Mohamed Saber <mmms77990@gmail.com>
2025-08-04 07:34:13 +0300

    add technician nav-link in navbar component with routing

--- D2D-Front/src/app/app.routes.ts
+++ D2D-Front/src/app/app.routes.ts
@@ -2,16 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../Components/Landing/home/home.component';
 import { LoginComponent } from '../Components/Landing/login/login.component';
 import { RegisterComponent } from '../Components/Landing/register/register.component';
-import { AppComponent } from './app.component';
 import { AboutComponent } from '../Components/Landing/about/about.component';
 import { FeedbackComponent } from '../Components/Landing/feedback/feedback.component';
 import { ServicesComponent } from '../Components/Landing/services/services.component';
+import { TechniciansComponent } from '../Components/Landing/technicians/technicians.component';
 
 export const routes: Routes = [
-  {path:'',component:HomeComponent},
-  {path:'login',component:LoginComponent},
-  {path:'signUp',component:RegisterComponent},
-  {path:'about',component:AboutComponent},
-  {path:'contact',component:FeedbackComponent},
-  {path:'services',component:ServicesComponent}
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signUp', component: RegisterComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: FeedbackComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'technicians', component: TechniciansComponent }
 ];
