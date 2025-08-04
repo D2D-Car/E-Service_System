a38e41b73ac58b5d8fc4d65a83dd1201b62dcba3
yomnakhatab <yomnakhatab87@gmail.com>
2025-08-04 12:31:45 +0300

    db

---
+++ D2D-Front/src/Components/technicians-dashboard/technicians-dashboard.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technicians-dashboard',
+  imports: [],
+  templateUrl: './technicians-dashboard.component.html',
+  styleUrl: './technicians-dashboard.component.css'
+})
+export class TechniciansDashboardComponent {
+
+}
