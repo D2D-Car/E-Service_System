d8d9cb7718adeeb7fc89376d3abac3092723f2bf
Yousef Elbilkasy <Yousuf_1241a@fci.kfs.edu.eg>
2025-08-06 23:19:30 +0300

    remove unused component imports from dashboard-home component add closing curly bracket that error code

--- D2D-Front/src/Components/Admin/dashboard-home/dashboard-home.component.ts
+++ D2D-Front/src/Components/Admin/dashboard-home/dashboard-home.component.ts
@@ -1,20 +1,11 @@
 import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { SalesSummaryCardsComponent } from '../sales-summary-cards/sales-summary-cards.component';
-import { TotalSellsChartComponent } from '../total-sells-chart/total-sells-chart.component';
-import { ProjectSummaryCardsComponent } from '../project-summary-cards/project-summary-cards.component';
-import { ProjectRoadmapComponent } from '../project-roadmap/project-roadmap.component';
-import { EarlyBirdLeadsCardComponent } from '../early-bird-leads-card/early-bird-leads-card.component';
 
 @Component({
   selector: 'app-dashboard-home',
   standalone: true,
   imports: [
     CommonModule,
-    TotalSellsChartComponent,
-    ProjectSummaryCardsComponent,
-    ProjectRoadmapComponent,
-    EarlyBirdLeadsCardComponent,
   ],
   templateUrl: './dashboard-home.component.html',
   styleUrls: ['./dashboard-home.component.css'],
