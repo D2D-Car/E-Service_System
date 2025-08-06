f196544f171557d731f1e747f6d201881b00cabb
Mohamed Saber <mmms77990@gmail.com>
2025-08-06 14:16:25 +0300

    solving conflict

--- D2D-Front/src/Components/Admin/dashboard-home/dashboard-home.component.ts
+++ D2D-Front/src/Components/Admin/dashboard-home/dashboard-home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SalesSummaryCardsComponent } from '../sales-summary-cards/sales-summary-cards.component';
 import { TotalSellsChartComponent } from '../total-sells-chart/total-sells-chart.component';
 import { ProjectSummaryCardsComponent } from '../project-summary-cards/project-summary-cards.component';
 import { ProjectRoadmapComponent } from '../project-roadmap/project-roadmap.component';
@@ -11,7 +10,6 @@ import { EarlyBirdLeadsCardComponent } from '../early-bird-leads-card/early-bird
   standalone: true,
   imports: [
     CommonModule,
-    SalesSummaryCardsComponent,
     TotalSellsChartComponent,
     ProjectSummaryCardsComponent,
     ProjectRoadmapComponent,
