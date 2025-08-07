900994330f471aee5c2d90458782b27822b912f2
Mohamed Saber <mmms77990@gmail.com>
2025-08-07 15:18:27 +0300

    making signout button in dashboards

--- D2D-Front/src/Components/Landing/navbar/navbar.component.ts
+++ D2D-Front/src/Components/Landing/navbar/navbar.component.ts
@@ -1,10 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterModule],
+  imports: [RouterModule,CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  constructor(public router: Router) { }
+
+
+  isDashboardRoute(): boolean {
+    const url = this.router.url;
+    return (
+      url.includes('/customer') ||
+      url.includes('/driver') ||
+      url.includes('/technician')
+    );
+  }
+}
