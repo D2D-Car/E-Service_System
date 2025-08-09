b84d19eea741e62ff5156dcebd2eb093e138acc1
Abduallh5Mohamed <abduallhshadow@gmail.com>
2025-08-09 17:58:10 +0300

    ....

--- D2D-Front/src/environments/environment.ts
+++ D2D-Front/src/environments/environment.ts
@@ -4,17 +4,17 @@ declare var process: any;
 export const environment = {
   production: false,
   firebase: {
-    apiKey: process?.env?.['NG_APP_FIREBASE_API_KEY'] || 'AIzaSyBef27wt27Y59ko3iTHE97Ap2UL-DpOhR0',
-    authDomain: process?.env?.['NG_APP_FIREBASE_AUTH_DOMAIN'] || 'd2ddatabase.firebaseapp.com',
-    databaseURL: process?.env?.['NG_APP_FIREBASE_DATABASE_URL'] || 'https://d2ddatabase-default-rtdb.firebaseio.com',
-    projectId: process?.env?.['NG_APP_FIREBASE_PROJECT_ID'] || 'd2ddatabase',
-    storageBucket: process?.env?.['NG_APP_FIREBASE_STORAGE_BUCKET'] || 'd2ddatabase.firebasestorage.app',
-    messagingSenderId: process?.env?.['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || '49961156364',
-    appId: process?.env?.['NG_APP_FIREBASE_APP_ID'] || '1:49961156364:web:40dd7d0207127ca2d65329',
-    measurementId: process?.env?.['NG_APP_FIREBASE_MEASUREMENT_ID'] || 'G-XS6P2WGWNY',
+  apiKey: process?.env?.['NG_APP_FIREBASE_API_KEY'] || '',
+  authDomain: process?.env?.['NG_APP_FIREBASE_AUTH_DOMAIN'] || '',
+  databaseURL: process?.env?.['NG_APP_FIREBASE_DATABASE_URL'] || '',
+  projectId: process?.env?.['NG_APP_FIREBASE_PROJECT_ID'] || '',
+  storageBucket: process?.env?.['NG_APP_FIREBASE_STORAGE_BUCKET'] || '',
+  messagingSenderId: process?.env?.['NG_APP_FIREBASE_MESSAGING_SENDER_ID'] || '',
+  appId: process?.env?.['NG_APP_FIREBASE_APP_ID'] || '',
+  measurementId: process?.env?.['NG_APP_FIREBASE_MEASUREMENT_ID'] || '',
   },
   recaptcha: {
-    siteKey: process?.env?.['NG_APP_RECAPTCHA_SITE_KEY'] || 'your-recaptcha-site-key',
+  siteKey: process?.env?.['NG_APP_RECAPTCHA_SITE_KEY'] || '',
   },
   // Add these options to handle connection issues
   firebaseOptions: {
